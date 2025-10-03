47e0db2f997226f5401b526e9c229edea7beb44e
N-Mehdi <mehdineji14@gmail.com>
2025-10-03 09:12:11 +0000

    modifs

--- src/business_object/utilisateur.py
+++ src/business_object/utilisateur.py
@@ -20,28 +20,25 @@ class Utilisateur:
         date de naissance de l'utilisateur
     """
 
+    def __init__(
+        self,
+        pseudo: str,
+        mail: str,
+        date_naissance: str,
+        mdp: str | None = None,
+        id_joueur: id | None = None,
+    ):
+        """Constructeur."""
+        self.id_joueur = id_joueur
+        self.pseudo = pseudo
+        self.mail = mail
+        self.date_naissance = date_naissance
+        self.mdp = mdp
 
-def __init__(
-    self,
-    pseudo: str,
-    mail: str,
-    date_naissance: str,
-    mdp: str | None = None,
-    id_joueur: id | None = None,
-) -> Utilisateur:
-    """Constructeur."""
-    self.id_joueur = id_joueur
-    self.pseudo = pseudo
-    self.mail = mail
-    self.date_naissance = date_naissance
-    self.mdp = mdp
+    def __str__(self) -> str:
+        """Afficher les informations de l'utilisateur."""
+        return f"Utilisateur({self.pseudo}, {self.date_naissance})"
 
-
-def __str__(self) -> str:
-    """Afficher les informations de l'utilisateur."""
-    return f"Utilisateur({self.pseudo}, {self.date_naissance})"
-
-
-def as_list(self) -> list[str]:
-    """Retourne pseudo, mail et date_naissance de l'utilsateur dans une liste."""
-    return [self.pseudo, self.mail, self.date_naissance]
+    def as_list(self) -> list[str]:
+        """Retourne pseudo, mail et date_naissance de l'utilsateur dans une liste."""
+        return [self.pseudo, self.mail, self.date_naissance]
