1f3d251d0c39b32f7b7b9ed890f99166c194bdbe
rb5gi <raphael.benichou.2004@gmail.com>
2025-11-04 13:42:12 +0000

    stock_coure_dao

--- src/dao/stock_course_dao.py
+++ src/dao/stock_course_dao.py
@@ -34,9 +34,11 @@ def get_stock(
         with connection.cursor() as cursor:
             cursor.execute(
                 """
-                VALUES (stock.id_stock)
-                RETURNING (id_ingredient,quantite,id_unite);
+                SELECT id_ingredient, quantite, id_unite
+                FROM stock
+                WHERE id_stock = %(id_stock)s;
                 """,
+                {"id_stock": stock.id_stock},
             )
             res = cursor.fetchone()
     return res
@@ -44,7 +46,6 @@ def get_stock(
 
 @log
 def update_ingredient_stock(id_ingredient, quantite, id_unite):
-    # rien n'est fait encore
     """Méthode qui permet de modifier la quantité d'un ingredient dans le stock.
 
     Parameters
@@ -53,7 +54,7 @@ def update_ingredient_stock(id_ingredient, quantite, id_unite):
         Identifiant unique de l'ingrédient
     quantite : float
         Nouvelle quantité de l'ingrédient
-    id_unité : int
+    id_unite : int
         Identifiant unique de l'unité de mesure
 
     Returns
@@ -88,8 +89,9 @@ def update_ingredient_stock(id_ingredient, quantite, id_unite):
 
 
 @log
-def delete_ingredient_stock(id_ingredient, qte, id_unite):
-    """Méthode qui permet de modifier la quantité d'un ingredient dans le stock.
+def delete_ingredient_stock(id_ingredient, quantite, id_unite):
+    """Méthode qui permet de supprimer une certaine quantité d'un ingredient dans
+     le stock.
 
     Parameters
     ----------
@@ -97,7 +99,7 @@ def delete_ingredient_stock(id_ingredient, qte, id_unite):
         Identifiant unique de l'ingrédient
     quantite : float
         Nouvelle quantité de l'ingrédient
-    id_unité : int
+    id_unite : int
         Identifiant unique de l'unité de mesure
 
     Returns
@@ -130,5 +132,124 @@ def delete_ingredient_stock(id_ingredient, qte, id_unite):
 
     return updated
 
+
 @log
-def 
+def get_liste_course(
+    utilisateur,
+):
+    """Méthode qui permet de renvoyer la liste de course d'un utilisateur.
+
+    Parameters
+    ----------
+    utilisateur : Utilisateur
+    objet de classe utilisateur
+
+    Returns
+    -------
+    liste de course : ?
+        La liste de course de l'utilisateur
+
+    """
+    res = None
+    with DBConnection().connection as connection:
+        with connection.cursor() as cursor:
+            cursor.execute(
+                """
+                SELECT id_ingredient, quantite, id_unite
+                FROM liste_course
+                WHERE id_utilisateur = %(id_utilisateur)s;
+                """,
+                {"id_utilisateur": utilisateur.id_utilisateur},
+            )
+            res = cursor.fetchone()
+    return res
+
+
+@log
+def updtate_liste_course(id_ingredient, quantite, id_unite):
+    """Méthode qui permet de modifier la quantité d'un ingredient dans la liste de
+     course.
+
+    Parameters
+    ----------
+    id_ingredient : int
+        Identifiant unique de l'ingrédient
+    quantite : float
+        Nouvelle quantité de l'ingrédient
+    id_unite : int
+        Identifiant unique de l'unité de mesure
+
+    Returns
+    -------
+    created : bool
+        renvoie si la quantité a bien était modifiée et vaut la grandeur souhaitée
+
+    """
+    try:
+        with DBConnection().connection as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    """
+                    UPDATE liste_course
+                    SET quantite = %(quantite)s, id_unite = %(id_unite)s
+                    WHERE id_ingredient = %(id_ingredient)s
+                    RETURNING id_ingredient;
+                    """,
+                    {
+                        "quantite": quantite,
+                        "id_unite": id_unite,
+                        "id_ingredient": id_ingredient,
+                    },
+                )
+                res = cursor.fetchone()
+                if res:
+                    updated = True
+    except Exception as e:
+        logging.info(e)
+
+    return updated
+
+
+@log
+def delete_ingredient_liste_course(id_ingredient, quantite, id_unite):
+    """Méthode qui permet de supprimer une certaine quantité d'un ingredient dans
+     la liste de course.
+
+    Parameters
+    ----------
+    id_ingredient : int
+        Identifiant unique de l'ingrédient
+    quantite : float
+        Nouvelle quantité de l'ingrédient
+    id_unite : int
+        Identifiant unique de l'unité de mesure
+
+    Returns
+    -------
+    created : bool
+        renvoie si la quantité a bien était modifiée et vaut la grandeur souhaitée
+
+    """
+    try:
+        with DBConnection().connection as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    """
+                    UPDATE stock
+                    SET quantite = quantite - %(quantite)s, id_unite = %(id_unite)s
+                    WHERE id_ingredient = %(id_ingredient)s
+                    RETURNING id_ingredient;
+                    """,
+                    {
+                        "quantite": quantite,
+                        "id_unite": id_unite,
+                        "id_ingredient": id_ingredient,
+                    },
+                )
+                res = cursor.fetchone()
+                if res:
+                    updated = True
+    except Exception as e:
+        logging.info(e)
+
+    return updated
