d0f3d4dbc49adc857e2d7ce47aad96121b1cfabb
maiwennpelle <maiwenn.pelle@eleve.ensai.fr>
2025-11-05 13:27:25 +0000

    test rechercher cocktail par nom ok

--- src/test/test_dao/test_cocktail_dao.py
+++ src/test/test_dao/test_cocktail_dao.py
@@ -1,15 +1,19 @@
 # import pytest
+
 from src.business_object.cocktail import Cocktail
 from src.dao.cocktail_dao import CocktailDao
 
 
 class TestCocktailDao:
-    def test_rechercher_cocktail_par_nom(self):
+    # Tests pour rechercher_cocktail_par_nom
+
+    def test_rechercher_cocktail_par_nom_succes(self):
         # GIVEN
         nom = "Margarita"
 
         # WHEN
-        resultat = CocktailDao.rechercher_cocktail_par_nom(nom)
+        dao = CocktailDao()
+        resultat = dao.rechercher_cocktail_par_nom(nom)
 
         # THEN
         cocktail_attendu = Cocktail(
@@ -21,3 +25,14 @@ class TestCocktailDao:
             image="https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg",
         )
         assert resultat == cocktail_attendu
+
+    def test_rechercher_cocktail_par_nom_non_trouve(self):
+        # GIVEN
+        nom = "Margaritaa"
+
+        # WHEN
+        dao = CocktailDao()
+        resultat = dao.rechercher_cocktail_par_nom(nom)
+
+        # THEN
+        assert resultat is None
